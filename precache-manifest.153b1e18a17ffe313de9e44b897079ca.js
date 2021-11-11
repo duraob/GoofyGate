32dce5dc183c7fa1f4b63d6fd92a4271b7a89fc5
jet <duraoben@gmail.com>
2021-11-10 22:03:52 -0500

    Updates

---
+++ precache-manifest.153b1e18a17ffe313de9e44b897079ca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f6299c194cd0cc1032888d3cf9d609",
+    "url": "/GoofyGate/index.html"
+  },
+  {
+    "revision": "a13b9868fb3df27f526d",
+    "url": "/GoofyGate/static/css/main.625d63ab.chunk.css"
+  },
+  {
+    "revision": "b7842bcbc92172c69e08",
+    "url": "/GoofyGate/static/js/2.a9a96578.chunk.js"
+  },
+  {
+    "revision": "a13b9868fb3df27f526d",
+    "url": "/GoofyGate/static/js/main.3ef96b7b.chunk.js"
+  },
+  {
+    "revision": "169907b60aa3484949fc",
+    "url": "/GoofyGate/static/js/runtime-main.f3e7e794.js"
+  }
+]);
